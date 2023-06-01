1cbc0679b9391f6e5e2c98392aa41ab58c40fa15
XanderBurger <86130720+XanderBurger@users.noreply.github.com>
2023-05-31 23:14:26 -0600

    first commit

---
+++ public/cleanData.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import numpy as np
+import os
+
+data = pd.read_csv("/Users/xanderburger/dataViz/toxicsReleaseInventory/site/public/2021_us.csv", low_memory=False)
+
+dataCondensed = data[["4. FACILITY NAME", 
+                      "34. CHEMICAL",
+                      "62. ON-SITE RELEASE TOTAL", 
+                      "51. 5.4 - UNDERGROUND",
+                      "40. CLASSIFICATION",
+                      "41. METAL",
+                      "43. CARCINOGEN",
+                      "44. PBT",
+                      "45. PFAS",
+                      "47. UNIT OF MEASURE",
+                      "52. 5.4.1 - UNDERGROUND CL I",
+                      "53. 5.4.2 - UNDERGROUND C II-V",
+                      "71. 6.2 - M71",
+                      "72. 6.2 - M81",
+                      "73. 6.2 - M82"]]
+
+global count
+count = 0
+def convertToPounds(df):
+    if df["47. UNIT OF MEASURE"] == "Grams":
+        df["51. 5.4 - UNDERGROUND"] *= 0.0022
+        df["52. 5.4.1 - UNDERGROUND CL I"] *= 0.0022
+        df["53. 5.4.2 - UNDERGROUND C II-V"] *= 0.0022
+        df["71. 6.2 - M71"] *= 0.0022
+        df["72. 6.2 - M81"] *= 0.0022
+        df["73. 6.2 - M82"] *= 0.0022
+        global count
+        count += 1
+
+    return df
+
+dataCondensedPounds = dataCondensed.apply(convertToPounds, raw=True, axis=1, result_type="broadcast")
+print(count, "values changed to pounds")
